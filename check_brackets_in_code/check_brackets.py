411a20401c271999ac1f023b66290be200eb78d3
Kirill Vysokolov <kir2250@gmail.com>
2018-01-25 17:05:54 +0300

    homework 1.1

--- check_brackets_in_code/check_brackets.py
+++ check_brackets_in_code/check_brackets.py
@@ -1,13 +1,14 @@
 # python3
 
-import sys
+import unittest
 
 class Bracket:
     def __init__(self, bracket_type, position):
         self.bracket_type = bracket_type
-        self.position = position
 
-    def Match(self, c):
+        self.position = position + 1
+
+    def match(self, c):
         if self.bracket_type == '[' and c == ']':
             return True
         if self.bracket_type == '{' and c == '}':
@@ -16,17 +17,53 @@ class Bracket:
             return True
         return False
 
-if __name__ == "__main__":
-    text = sys.stdin.read()
+
+def program(text):
+    balance = None
 
     opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next == '(' or next == '[' or next == '{':
+    for i, nextb in enumerate(text):
+        if nextb == '(' or nextb == '[' or nextb == '{':
             # Process opening bracket, write your code here
-            pass
-
-        if next == ')' or next == ']' or next == '}':
+            opening_brackets_stack.append(Bracket(nextb, i))
+        if nextb == ')' or nextb == ']' or nextb == '}':
             # Process closing bracket, write your code here
-            pass
+            if len(opening_brackets_stack) == 0:
+                balance = i + 1
+                break
+            elif opening_brackets_stack[len(opening_brackets_stack) - 1].match(nextb):
+                opening_brackets_stack.pop()
+            else:
+                balance = i + 1
+                break
+                # Printing answer, write your code here
+    if opening_brackets_stack != [] and balance is None:
+        balance = opening_brackets_stack.pop().position
+    if balance is None:
+        return "Success"
+    else:
+        return str(balance)
 
-    # Printing answer, write your code here
+
+class MyTest(unittest.TestCase):
+
+    def testOne(self):
+        for x in range(1, 54):
+            if x < 10:
+                x = str(x)
+                x = '0'+x
+            else:
+                x = str(x)
+            print("Test", x, "... ", end="")
+            t = open('tests/'+x, 'r')
+            a = open('tests/'+x+'.a', 'r')
+            self.assertEqual(program(t.readline()), a.readline().rstrip())
+            print("successful.")
+            t.close()
+            a.close()
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
+    text = input()
+    print(program(text))

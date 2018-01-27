90ce406751a4d87b2ce23e10ce6f6a66b7e470ee
Kirill Vysokolov <kir2250@gmail.com>
2018-01-27 22:39:32 +0300

    hw1

--- network_packet_processing_simulation/process_packages.py
+++ network_packet_processing_simulation/process_packages.py
@@ -1,5 +1,7 @@
 # python3
 
+import unittest
+
 class Request:
     def __init__(self, arrival_time, process_time):
         self.arrival_time = arrival_time
@@ -16,27 +18,85 @@ class Buffer:
         self.finish_time_ = []
 
     def Process(self, request):
-        # write your code here
-        return Response(False, -1)
+        # # write your code here
+        while self.finish_time_:
+            if request.arrival_time >= self.finish_time_[0]:
+                self.finish_time_.remove(self.finish_time_[0])
+            else:
+                break
 
-def ReadRequests(count):
+        if len(self.finish_time_) >= self.size:
+            return Response(True, -1)
+
+        else:
+            if len(self.finish_time_) == 0:
+                self.finish_time_.append(request.arrival_time + request.process_time)
+                return Response(False, request.arrival_time)
+            else:
+                rtime = self.finish_time_[len(self.finish_time_) - 1]
+                if request.arrival_time >= rtime:
+                    self.finish_time_.append(request.arrival_time + request.process_time)
+                    return Response(False, request.arrival_time)
+                else:
+                    self.finish_time_.append(rtime + request.process_time)
+                    return Response(False, rtime)
+
+
+def ReadRequests(count, *args):
     requests = []
-    for i in range(count):
-        arrival_time, process_time = map(int, input().strip().split())
-        requests.append(Request(arrival_time, process_time))
+    if args:
+        tfile = args[0]
+        for i in range(count):
+            arrival_time, process_time = map(int, tfile.readline().strip().split())
+            requests.append(Request(arrival_time, process_time))
+    else:
+        for i in range(count):
+            arrival_time, process_time = map(int, input().strip().split())
+            requests.append(Request(arrival_time, process_time))
     return requests
 
 def ProcessRequests(requests, buffer):
     responses = []
     for request in requests:
-        responses.append(buffer.Process(request))
+        response = buffer.Process(request)
+        if response:
+            responses.append(response)
+    # while buffer.finish_time_:
+    #     responses.append(buffer.emptyQueue())
     return responses
 
 def PrintResponses(responses):
     for response in responses:
         print(response.start_time if not response.dropped else -1)
 
+class MyTest(unittest.TestCase):
+    def testOne(self):
+        for x in range(1, 23):
+            if x < 10:
+                x = str(x)
+                x = '0'+x
+            else:
+                x = str(x)
+            print("Test", x, "... ", end="")
+            t = open('tests/'+x, 'r')
+            a = open('tests/'+x+'.a', 'r')
+            tsize, tcount = map(int, t.readline().strip().split())
+            trequests = ReadRequests(tcount, t)
+            testoutput = str()
+            tbuffer = Buffer(tsize)
+            tresponses = ProcessRequests(trequests, tbuffer)
+            for i, response in enumerate(tresponses):
+                testoutput += str(response.start_time if not response.dropped else -1)
+                if i >= 0:
+                    testoutput += "\n"
+            self.assertEqual(testoutput, a.read())
+            print("successful.")
+            t.close()
+            a.close()
+
+
 if __name__ == "__main__":
+    unittest.main(verbosity=2)
     size, count = map(int, input().strip().split())
     requests = ReadRequests(count)
 
--- tree_height/tree-height.py
+++ tree_height/tree-height.py
@@ -1,29 +1,72 @@
 # python3
 
 import sys, threading
+#import unittest
 sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
 
-class TreeHeight:
-        def read(self):
-                self.n = int(sys.stdin.readline())
-                self.parent = list(map(int, sys.stdin.readline().split()))
 
-        def compute_height(self):
-                # Replace this code with a faster implementation
-                maxHeight = 0
-                for vertex in range(self.n):
-                        height = 0
-                        i = vertex
-                        while i != -1:
-                                height += 1
-                                i = self.parent[i]
-                        maxHeight = max(maxHeight, height);
-                return maxHeight;
+class TreeHeight:
+    def __init__(self):
+        self.n = 0
+        self.parent = list()
+        self.nodes = []
+        self.root = int()
+
+    def read(self, *args):
+        if args:
+            self.n = int(args[0])
+            self.parent = list(map(int, args[1].split()))
+        else:
+            self.n = int(sys.stdin.readline())
+            self.parent = list(map(int, sys.stdin.readline().split()))
+        self.nodes = [[] for _ in range(self.n)]
+
+    def build_tree(self):
+        for i, parent_index in enumerate(self.parent):
+            if parent_index == -1:
+                self.root = i
+            else:
+                self.nodes[parent_index].append(i)
+        # print(self.nodes)
+
+    def compute_height(self, *args):
+        # Replace this code with a faster implementation
+        if args:
+            index = args[0]
+        else:
+            index = self.root
+        if not self.nodes[index]:
+            return 1
+        return 1 + max(self.compute_height(_) for _ in self.nodes[index])
+
+
+# class MyTest(unittest.TestCase):
+#     def testOne(self):
+#         tree = TreeHeight()
+#         for x in range(1, 25):
+#             if x < 10:
+#                 x = str(x)
+#                 x = '0'+x
+#             else:
+#                 x = str(x)
+#             print("Test", x, "... ", end="")
+#             t = open('tests/'+x, 'r')
+#             a = open('tests/'+x+'.a', 'r')
+#             tree.read(t.readline(), t.readline())
+#             tree.build_tree()
+#             self.assertEqual(str(tree.compute_height()), a.readline().rstrip())
+#             print("successful.")
+#             t.close()
+#             a.close()
+
 
 def main():
-  tree = TreeHeight()
-  tree.read()
-  print(tree.compute_height())
+    # unittest.main(verbosity=2)
+    tree = TreeHeight()
+    tree.read()
+    tree.build_tree()
+    print(tree.compute_height())
+
 
 threading.Thread(target=main).start()
